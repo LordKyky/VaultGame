c17955200d9f378de21c090e67c59644989a1aa4
LordKyky <luxeon06@gmail.com>
2024-10-09 01:50:06 +0300

    feat(ui): added scaled and centered handle and handleShadow sprite texture to scene.

--- src/main.ts
+++ src/main.ts
@@ -13,26 +13,6 @@ gsap.registerPlugin(PixiPlugin);
 var size = [1920, 1080];
 var ratio = size[0] / size[1];
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
 (async () =>
   {
     // Create a new application
@@ -49,36 +29,82 @@ var ratio = size[0] / size[1];
         // Add the assets to load
         Assets.add({ alias: 'bg', src: 'assets/bg.png' });
         Assets.add({ alias: 'door', src: 'assets/door.png' });
+        Assets.add({ alias: 'handleShadow', src: 'assets/handleShadow.png' });
+        Assets.add({ alias: 'handle', src: 'assets/handle.png' });
     
         // Start loading right away and create a promise
         //const texturePromise = Assets.load('https://pixijs.com/assets/bunny.png');
   
         // Load the assets and get a resolved promise once both are loaded
-        const texturesPromise = Assets.load(['bg', 'door']); // => Promise<{flowerTop: Texture, eggHead: Texture}>
+        const texturesPromise = Assets.load(['bg', 'door', 'handleShadow', 'handle']); // => Promise<{flowerTop: Texture, eggHead: Texture}>
   
     
-        // When the promise resolves, we have the texture!
+        // When the promise resolves, we have the textures!
         texturesPromise.then((textures) =>
         {
-          // // create a new Sprite from the resolved loaded Texture
-          // const bunny = Sprite.from(resolvedTexture);
-          
-          // // center the sprite's anchor point
-          // bunny.anchor.set(0.5);
-          
-          // // move the sprite to the center of the screen
-          // bunny.x = app.screen.width / 2;
-          // bunny.y = app.screen.height / 2;
-          
-          // app.stage.addChild(bunny);
-          
+          // Create a new Sprite from the resolved loaded Texture for background
           const bg = Sprite.from(textures.bg);
+
+          // Center the sprite's anchor point
           bg.anchor.set(0.5);
+
+          // Move the background sprite to the center of the screen
           bg.x = app.screen.width / 2;
           bg.y = app.screen.height / 2;
+
+          // Set height and width of background to screen size
           bg.width = app.screen.width;
           bg.height = app.screen.height;
+
           app.stage.addChild(bg);
+
+          // Create a new Sprite from the resolved loaded Texture for door
+          const door = Sprite.from(textures.door);
+
+          // Center the sprite's anchor point
+          door.anchor.set(0.5);
+
+          // Move the door sprite to the center of the screen
+          door.x = app.screen.width / 2 + 14;
+          door.y = app.screen.height / 2 - 9.5;
+
+          // Set height and width of door to the screen size
+          door.width = app.screen.width / 3;
+          door.height = app.screen.height / 1.55;
+
+          app.stage.addChild(door);
+
+          // Create a new Sprite from the resolved loaded Texture for handle shadow
+          const handleShadow = Sprite.from(textures.handleShadow);
+
+          // Center the sprite's anchor point
+          handleShadow.anchor.set(0.5);
+
+          // Move the handle sprite to the center of the door sprite
+          handleShadow.x = door.x / 1.027;
+          handleShadow.y = door.y * 1.03;
+
+          // Set height and width of handle shadow to the door size
+          handleShadow.width = door.width / 2.85;
+          handleShadow.height = door.height / 2.6;
+
+          app.stage.addChild(handleShadow);
+
+          // Create a new Sprite from the resolved loaded Texture for handle shadow
+          const handle = Sprite.from(textures.handle);
+
+          // Center the sprite's anchor point
+          handle.anchor.set(0.5);
+
+          // Move the handle sprite to the center of the door sprite
+          handle.x = door.x / 1.027;
+          handle.y = door.y;
+
+          // Set height and width of handle shadow to the door size
+          handle.width = door.width / 2.85;
+          handle.height = door.height / 2.6;
+
+          app.stage.addChild(handle);
         });
       });
   })();
